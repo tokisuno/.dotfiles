3fbb683fae93ada964348ca74e9da458e143f427
tokisuno <lucaswmei@gmail.com>
2024-06-01 15:47:13 -0400

    things

--- init.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-cd ~/Dropbox/.dotfiles
-git init
-git remote add origin https://github.com/tokisuno/.dotfiles
-git config user.email "lucaswmei@gmail.com" && git config user.name "tokisuno"
-git add .
-git commit -m 'things'
-git push -u origin main --force
