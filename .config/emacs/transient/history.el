846f56361b6f556efd74b2bdd1b4e23afb14b2b0
tokisuno <lucaswmei@gmail.com>
2024-06-18 10:24:35 -0400

    removed emacs

--- .config/emacs/transient/history.el
+++
@@ -1,4 +0,0 @@
-((emoji--command-Emoji nil)
- (emoji--command-Emoji\ >\ Food\ &\ Drink nil)
- (emoji--command-Emoji\ >\ Food\ &\ Drink\ >\ food nil)
- (emoji--command-Emoji\ >\ Food\ &\ Drink\ >\ food\ >\ fruit nil))
